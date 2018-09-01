5b3a4846d7d7d70171d2dadddf50edda3c7aaf28
Koluna7891 <darkman7891@mail.ru>
2018-09-01 16:32:50 +0300

    Arifmetics

--- sandbox/src/main/java/ru_id/stqa/prog/sandbox/Prog.java
+++ sandbox/src/main/java/ru_id/stqa/prog/sandbox/Prog.java
@@ -4,6 +4,12 @@ public class Prog {
 
   public static void main(String[] args) {
     System.out.println("hi, gay");
+
+    int h = 5;
+    int u = h * h;
+
+    System.out.println("Площадь квадрата со стророной " + h + " = " + u);
+
   }
 
 }
