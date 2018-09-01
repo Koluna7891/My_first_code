423e1514ab12504d6d1c1bf3dd7396526942c5fc
Koluna7891 <darkman7891@mail.ru>
2018-09-01 12:29:58 +0300

    Change to config

--- sandbox/src/main/java/ru_id/stqa/prog/sandbox/Prog.java
+++ sandbox/src/main/java/ru_id/stqa/prog/sandbox/Prog.java
@@ -2,8 +2,8 @@ package ru_id.stqa.prog.sandbox;
 
 public class Prog {
 
-public static void main(String[] args) {
-System.out.println("hi, gay");
-}
+  public static void main(String[] args) {
+    System.out.println("hi, gay");
+  }
 
 }
